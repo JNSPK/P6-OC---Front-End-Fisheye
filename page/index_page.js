eeaa2397f99f2e267bc048b201cb736b8965cd1c
Johan Nilsson <johan.nilsson.mail@gmail.com>
2023-06-13 14:38:25 +0200

    fix url

--- page/index_page.js
+++ page/index_page.js
@@ -1,5 +1,5 @@
-import PhotographerApi from './scripts/api/photographers_api.js';
-import PhotographerCard from './scripts/builder/photographerCard_builder.js';
+import PhotographerApi from '/scripts/api/photographers_api.js';
+import PhotographerCard from '/scripts/builder/photographerCard_builder.js';
 
 new PhotographerApi()
   .getPhotographers()
