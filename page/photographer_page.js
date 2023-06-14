d2ae49d0443fda11be22b7d687073ff18b856ace
Johan Nilsson <johan.nilsson.mail@gmail.com>
2023-06-14 13:43:13 +0200

    fix URL

--- page/photographer_page.js
+++ page/photographer_page.js
@@ -2,8 +2,8 @@ import PhotographerApi from './../scripts/api/photographers_api.js';
 import PhotographerInfos from './../scripts/builder/photographerInfos_builder.js';
 import MediaCard from './../scripts/builder/mediaCard_builder.js';
 import Carousel from './../scripts/builder/carousel_builder.js';
-import FilterEvent from './scripts/eventListener/filter_eventListener.js';
-import LikesEvent from './../scripts/eventListener/likes_eventListener.js';
+import FilterEvent from '../scripts/eventListener/filter_eventListener.js';
+import LikesEvent from '../scripts/eventListener/likes_eventListener.js';
 
 // Récupération de l'ID du photographe dans l'URL
 const params = new URL(document.location).searchParams;
