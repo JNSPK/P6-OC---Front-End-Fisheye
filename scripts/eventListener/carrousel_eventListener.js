62722aac556888071ea3de682051dba4edf42e01
Johan Nilsson <johan.nilsson.mail@gmail.com>
2023-06-20 13:49:56 +0200

    Feature : début de mise en place pour l'accessibilité (fenêtre modale, naviguation au clavier et aria-hidden,label)

--- scripts/eventListener/carrousel_eventListener.js
+++ scripts/eventListener/carrousel_eventListener.js
@@ -1,37 +1,64 @@
 export default class CarrouselListener {
   static listen() {
     this.listenImg();
+    const carrousel = document.querySelector('.carrousel-wrapper');
     const prevButton = document.getElementById('slide-arrow-prev');
     const nextButton = document.getElementById('slide-arrow-next');
-
-    const carrousel = document.querySelector('.carrousel-wrapper');
     const closeButton = document.querySelector('.close');
+
     nextButton.addEventListener('click', () => {
-      const slideWidth = carrousel.clientWidth;
-      carrousel.scrollLeft += slideWidth;
-      console.log();
+      next();
     });
 
     prevButton.addEventListener('click', () => {
-      const slideWidth = carrousel.clientWidth;
-      carrousel.scrollLeft -= slideWidth;
+      previous();
     });
 
     closeButton.addEventListener('click', () => {
       carrousel.scrollLeft = 0;
     });
+
+    carrousel.keydown((e) => {
+      const keyCode = e.keyCode ? e.keyCode : e.which;
+
+      if (keyCode === 39) {
+        next();
+      } else if (keyCode === 37) {
+        previous();
+      } else if (keyCode === 27) {
+        const overlay = document.querySelector('.overlay');
+        overlay.classList.toggle('active');
+        carrousel.scrollLeft = 0;
+      }
+    });
+    function next() {
+      const slideWidth = carrousel.clientWidth;
+      carrousel.scrollLeft += slideWidth;
+    }
+    function previous() {
+      const slideWidth = carrousel.clientWidth;
+      carrousel.scrollLeft -= slideWidth;
+    }
   }
 
   static async listenImg() {
-    document.querySelector('main').addEventListener('click', (e) => {
+    document.querySelector('body').addEventListener('click', (e) => {
+      const attrToggle = (el, attr) =>
+        el.getAttribute(attr) == 'false'
+          ? el.setAttribute(attr, 'true')
+          : el.setAttribute(attr, 'false');
+
+      const main = document.querySelector('main');
+      const overlay = document.querySelector('.overlay');
       const isACarrouselTrigger =
         e.target.classList.contains('carrousel-trigger');
-      const overlay = document.querySelector('.overlay');
       const all = Array.from(document.querySelectorAll('.carrousel-trigger'));
       const targetIndex = all.indexOf(e.target);
 
       if (isACarrouselTrigger && targetIndex < 12 && targetIndex >= 0) {
         overlay.classList.toggle('active');
+        attrToggle(overlay, ['aria-hidden']);
+        attrToggle(main, ['aria-hidden']);
 
         const carrousel = document.querySelector('.carrousel-wrapper');
         const slideWidth = carrousel.offsetWidth;
