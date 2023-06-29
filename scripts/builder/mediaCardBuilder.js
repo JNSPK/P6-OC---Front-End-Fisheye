105648cd3ca5b6dc89d34dabb3450258a1c3fb76
Johan Nilsson <johan.nilsson.mail@gmail.com>
2023-06-29 16:41:52 +0200

    Amélioration de l'accessibilité pour lecteur d'écran

--- scripts/builder/mediaCardBuilder.js
+++ scripts/builder/mediaCardBuilder.js
@@ -14,7 +14,7 @@ export default class MediaCard {
 	static buildPhoto(media) {
 		return `
    <article class="card-photo-wrapper">
-      <section class="img-wrapper" tabindex="4">
+      <section class="img-wrapper" tabindex="4" aria-label="agrandir l'image">
           <img class="carrousel-trigger" 
             src="assets/images/FishEye_Photos/Sample Photos/Photographers ID Photos/${media.photographerId}/${media.image}"
             alt="Illustration du photographe" aria-label="agrandir l'image">
@@ -32,7 +32,7 @@ export default class MediaCard {
 	static buildVideo(media) {
 		return `
    <article class="card-photo-wrapper">
-      <section class="img-wrapper" tabindex="4">
+      <section class="img-wrapper" tabindex="4" aria-label="agrandir la video">
           <video class="carrousel-trigger video"   autoplay muted loop 
             src="assets/images/FishEye_Photos/Sample Photos/Photographers ID Photos/${media.photographerId}/${media.video}"
             alt="Illustration du photographe" aria-label="agrandir la video">
